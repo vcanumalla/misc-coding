044ad0be509fb1ec71e122b2913471056351de1b
vishal <vishalcan@hotmail.com>
2020-04-23 21:05:52 -0700

    permutations

--- cses/intro/permutations.java
+++ cses/intro/permutations.java
@@ -2,16 +2,19 @@ import java.util.*;
 public class permutations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        if (n < 5) {
+        int n = sc.nextInt();
+        if ( n== 1) {
+            System.out.println(n);
+        }
+        else if (n < 4) {
             System.out.println("NO SOLUTION");
         }
         else {
-            for (long i = n; i > 0; i -=2) {
+            for (int i = 2; i <= n; i+=2) {
                 System.out.print(i + " ");
             }
-            for (long i = n -1; i > 0; i -=2) {
-                System.out.print(i + " ");
+            for (int i = 1; i <= n; i+=2) {
+                System.out.print(i + " "); 
             }
         }
     }
