cefd4c9a436a49780964537c45bf680044bbcf9d
Vishal C <vishalcan@hotmail.com>
2020-06-25 13:41:02 -0700

    1335

---
+++ comp/1335/A.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int> >;
+using vll = vector<long long>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using vpi = vector<pair<int, int> >;
+ 
+#define F first
+#define S second
+#define pb push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+ 
+#define FOR(i, a, b) for (int i=(a); i <=(b); i++)
+#define F0R(i, a) for (int i=0; i < (a); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= b; i--)
+ 
+const char nl = '\n';
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while (t--) {
+        int x, y; cin >> x >> y;
+        if (y < x) {
+            y *= 2;
+        }
+        else {
+            x *= 2;
+        }
+        int ar = max(x,y) * max(x,y);
+        cout << ar << nl;
+    }
+    return 0;
+ 
+}
