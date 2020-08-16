8f1702c2a00d656e11b1f4cf212febf714d14c57
Vishal C <vishalcan@hotmail.com>
2020-08-15 20:12:08 -0700

    deletions

---
+++ randos/B230.cpp
@@ -0,0 +1,69 @@
+
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int> >;
+using vll = vector<long long>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using vpi = vector<pair<int, int> >;
+
+#define F first
+#define S second
+#define pb push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+#define FOR(i, a, b) for (int i=(a); i <=(b); i++)
+#define F0R(i, a) for (int i=0; i < (a); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= b; i--)
+
+const char nl = '\n';
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+bool isPrime(ll n) 
+{ 
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (ll i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+} 
+  
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while (t--) {
+        ll n; cin >> n;
+        ll sq = (ll ) sqrt(n);
+        bool check = isPrime(sq);
+        if (sq * sq == n) {
+            if (check) {
+                cout << "YES" << nl;
+            }
+            else {
+                cout << "NO" << nl;
+            }
+        }
+        else {
+            cout << "NO" << nl;
+        }
+    }
+    return 0;
+    
+
+}
+
